4353a893d7ab5442299ec69b6a92a915a44fb4e4
Alberto Rivo <alberto.rivoira@edu.itspiemonte.it>
2025-01-12 20:17:36 +0100

    aggiunto contenuto semifinale email

--- src/main/java/com/codecraft/agora_backend/service/SendEmailService.java
+++ src/main/java/com/codecraft/agora_backend/service/SendEmailService.java
@@ -37,8 +37,14 @@ public class SendEmailService {
         if(adminEmails.isPresent()) {
             message.setFrom(adminEmails.get().getNoReplyEmail());
             message.setTo(formInfo.getEmail());
-            message.setSubject("Richiesta di informazioni a Cascina Caccia");
-            message.setText(formInfo.getName()+ " "+ formInfo.getSurname()+"\n"+formInfo.getAdditionalInfo());
+            message.setSubject("Cascina Caccia: conferma di ricezione della richiesta di informazioni");
+            message.setText("Ciao " + formInfo.getName() + ","
+                            + "\ngrazie per averci contattato e aver mostrato interesse nelle attività di Cascina Caccia dedicate a bambini e ragazzi."
+                            + "\nAbbiamo ricevuto la tua comunicazione di richiesta di informazioni. Risponderemo ai tuoi dubbi e alle tue domande entro 3 giorni lavorativi per permetterti di organizzare al meglio."
+                            + "\nNel frattempo ti invitiamo a iscriverti alla nostra newsletter gratuita per avere aggiornamenti sulle attività di Cascina Caccia e per ricevere materiale di formazione e approfondimento."
+                            + "\nPer iscriverti alla newsletter andare al seguente link: [Nome Link]"
+                            + "\nA presto!"
+                            + "\nI volontari di Cascina Caccia");
         }
         mailSender.send(message);
     }
@@ -50,8 +56,14 @@ public class SendEmailService {
         if(adminEmails.isPresent()) {
             message.setFrom(adminEmails.get().getNoReplyEmail());
             message.setTo(formBooking.getEmail());
-            message.setSubject("Richiesta di prenotazione a Cascina Caccia");
-            message.setText(formBooking.getName()+ " "+ formBooking.getSurname()+"\n"+formBooking.getAdditionalInfo());
+            message.setSubject("Conferma Prenotazione - Codice 235468");
+            message.setText("Ciao "+formBooking.getName() + ","
+                            + "\nsiamo lieti di confermare la prenotazione per il tuo gruppo in data da "+formBooking.getBeginTime()+" a "+formBooking.getEndTime()+". Il tuo codice di prenotazione è 235468. Lo potrai inserire nella sezione dedicata per modificare o correggere le informazioni che ci hai comunicato."
+                            + "\nNei prossimi giorni uno dei nostri volontari ti contatterà per discutere al meglio i dettagli della tua permanenza in Cascina."
+                            + "\nNel frattempo ti consigliamo di dare un'occhiata alla sezione FAQ del nostro sito per rispondere a eventuali dubbi o domande."
+                            + "\nPer modificare le informazioni inserite nel form andare al seguente link: [Nome Link]"
+                            + "\nTi aspettiamo in Cascina!"
+                            + "\nI volontari di Cascina Caccia");
         }
         mailSender.send(message);
     }
@@ -63,8 +75,19 @@ public class SendEmailService {
         if(adminEmails.isPresent()) {
             message.setFrom(adminEmails.get().getNoReplyEmail());
             message.setTo(adminEmails.get().getAdminEmail());
-            message.setSubject("Richiesta di informazioni da "+formInfo.getSurname());
-            message.setText(formInfo.getName()+ " "+ formInfo.getSurname()+"\n"+formInfo.getAdditionalInfo());
+            message.setSubject("Agorà - Nuova Richiesta Infomazioni (" + formInfo.getAssociation()+")");
+            message.setText("Gentili Amministratori,"
+                            + "\nÈ stata inviata una nuova richiesta di informazioni tramite il sito. Di seguito i dettagli:"
+                            + "\nDati del richiedente:"
+                            + "\n   • Nome e Cognome: "+formInfo.getName()+" "+formInfo.getSurname()
+                            + "\n   • Ente: "+formInfo.getAssociation()
+                            + "\n   • E-mail: "+formInfo.getEmail()
+                            + "\n   • Cellulare: "+formInfo.getPhoneNumber()
+                            + "\nDettagli della richiesta:"
+                            + "\n   • Attività: DOPO MERGE AGGIUNGERE CHIAMATA A ATTIVITà"
+                            + "\n   • Messaggio: " + formInfo.getAdditionalInfo()
+                            + "\nDettagli invio:"
+                            + "\n   • Richiesta effettuata il "+formInfo.getContactDate());
         }
         mailSender.send(message);
     }
@@ -76,8 +99,22 @@ public class SendEmailService {
         if(adminEmails.isPresent()) {
             message.setFrom(adminEmails.get().getNoReplyEmail());
             message.setTo(adminEmails.get().getAdminEmail());
-            message.setSubject("Richiesta di prenotazione da " + formBooking.getSurname());
-            message.setText(formBooking.getName()+ " "+ formBooking.getSurname()+"\n"+formBooking.getAdditionalInfo());
+            message.setSubject("Agorà: Nuova Prenotazione - Codice 235468");
+            message.setText("Gentili Amministratori," +
+                            "\nÈ stata effettuata una nuova prenotazione tramite il sito. Di seguito i dettagli:" +
+                            "\nDati del richiedente:" +
+                            "\n   • Nome e Cognome: "+formBooking.getName()+" "+formBooking.getSurname()+
+                            "\n   • Ente: "+formBooking.getAssociation()+
+                            "\n   • E-mail: "+formBooking.getEmail()+
+                            "\n   • Cellulare: "+formBooking.getPhoneNumber()+
+                            "\nInformazioni sulla visita:"+
+                            "\n   • Periodo di disponibilità: da "+formBooking.getBeginTime()+ " a "+formBooking.getEndTime()+
+                            "\n   • Numero bambini e ragazzi: "+formBooking.getParticipantsQuantity()+
+                            "\n   • Numero accompagnatori: "+formBooking.getGuidesQuantity()+
+                            "\n   • Attività scelte: "+formBooking.getActivityType()+
+                            "\n   • Messaggio: "+formBooking.getAdditionalInfo()+
+                            "\nDettagli prenotazione: "+
+                            "\n   • Prenotazione effettuata il "+formBooking.getContactDate());
         }
         mailSender.send(message);
     }
