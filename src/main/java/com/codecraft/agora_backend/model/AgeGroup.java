782a259cc4ea960fb4eb08a5cbcc228ecec858d0
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2025-01-16 13:22:32 +0100

    fix: minor fixes (deleted unused imports)

--- src/main/java/com/codecraft/agora_backend/model/AgeGroup.java
+++ src/main/java/com/codecraft/agora_backend/model/AgeGroup.java
@@ -1,7 +1,6 @@
 package com.codecraft.agora_backend.model;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
