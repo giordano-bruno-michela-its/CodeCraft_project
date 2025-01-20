3374e46de3e1a0d80b2fb4fe47348c5740ade42d
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2025-01-20 17:54:45 +0100

    fix: minor fixes, unused imports

--- src/main/java/com/codecraft/agora_backend/controller/UserController.java
+++ src/main/java/com/codecraft/agora_backend/controller/UserController.java
@@ -4,7 +4,6 @@ import com.codecraft.agora_backend.dto.UserDTO;
 import com.codecraft.agora_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
