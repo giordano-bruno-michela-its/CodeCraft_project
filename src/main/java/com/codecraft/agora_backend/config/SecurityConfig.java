ee564cb22ce6e45987a060887504632454bd3579
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2025-01-20 17:13:41 +0100

    fix: added JWT implementation on SwaggerUI

--- src/main/java/com/codecraft/agora_backend/config/SecurityConfig.java
+++ src/main/java/com/codecraft/agora_backend/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                     authorize.requestMatchers("/api/auth/**").permitAll();
                     authorize.requestMatchers("/api/formreq/create").permitAll();
                     authorize.requestMatchers("/api/formreq/createbooking").permitAll();
+                    authorize.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
                     authorize.requestMatchers("/api/**").hasAnyRole("ADMIN", "USER");
                     authorize.anyRequest().authenticated();
                 }).httpBasic(Customizer.withDefaults());
