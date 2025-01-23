08f5470346ed7fb0592ec477d0c006f79f1d11f9
Giordano Bruno Michela <giordano.michela@edu.itspiemonte.it>
2025-01-23 18:29:10 +0100

    docs: swagger AgeGroupController

--- src/main/java/com/codecraft/agora_backend/controller/AgeGroupController.java
+++ src/main/java/com/codecraft/agora_backend/controller/AgeGroupController.java
@@ -4,6 +4,8 @@ import com.codecraft.agora_backend.dto.AgeGroupDTO;
 import com.codecraft.agora_backend.model.View;
 import com.codecraft.agora_backend.service.AgeGroupService;
 import com.fasterxml.jackson.annotation.JsonView;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.Optional;
 /**
  * REST controller for managing age groups.
  */
+@Tag(name = "AgeGroupController", description = "Controller for managing age groups")
 @RestController
 @RequestMapping("/api/agegroup")
 @CrossOrigin(origins = "http://localhost:3000")
@@ -34,6 +37,7 @@ public class AgeGroupController {
      *
      * @return the list of all age groups
      */
+    @Operation(summary = "Get all age groups", description = "Retrieve a list of all age groups")
     @GetMapping("/all")
     @JsonView(View.GetView.class)
     public List<AgeGroupDTO> getAllAgeGroup() {
@@ -46,6 +50,7 @@ public class AgeGroupController {
      * @param id the age group ID
      * @return the age group with the given ID
      */
+    @Operation(summary = "Get age group by ID", description = "Retrieve an age group by its ID")
     @GetMapping("/{id}")
     @JsonView(View.GetView.class)
     public ResponseEntity<AgeGroupDTO> getAgeGroupById(@PathVariable Long id) {
@@ -59,6 +64,7 @@ public class AgeGroupController {
      * @param ageGroupDTO the age group to create
      * @return the created age group
      */
+    @Operation(summary = "Create new age group", description = "Create a new age group")
     @PostMapping("/create")
     @JsonView(View.SubView.class)
     public ResponseEntity<AgeGroupDTO> createAgeGroup(@RequestBody AgeGroupDTO ageGroupDTO) {
@@ -73,6 +79,7 @@ public class AgeGroupController {
      * @param ageGroupDTO the age group data to update
      * @return the updated age group
      */
+    @Operation(summary = "Update age group by ID", description = "Update an existing age group by its ID")
     @PutMapping("/update/{id}")
     @JsonView(View.SubView.class)
     public ResponseEntity<AgeGroupDTO> updateAgeGroup(@PathVariable Long id, @RequestBody AgeGroupDTO ageGroupDTO) {
@@ -90,6 +97,7 @@ public class AgeGroupController {
      * @param id the age group ID
      * @return no content response
      */
+    @Operation(summary = "Delete age group by ID", description = "Delete an age group by its ID")
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteAgeGroup(@PathVariable Long id) {
         ageGroupService.deleteAgeGroup(id);
